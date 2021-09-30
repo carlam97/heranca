d192e16690cf59ba064d01a945968c5fd54edff7
carlam97 <carmartins04@hotmail.com>
2021-09-30 19:05:18 +0000

    herança

--- Principal.java
+++ Principal.java
@@ -1,15 +1,15 @@
 public class Principal {
     public static void main(String args[]){
         Funcionario f=new Funcionario();
-        f.setNome("Carla");
+        f.setNome("Carla Martins");
         f.setCpf("123.456.789-00");
         f.setSalario(2500);
         f.setDepartamento("Financeiro");
 
-        System.out.println(f.getNome());
-        System.out.println(f.getCpf());
-        System.out.println(f.getSalario());
-        System.out.println(f.getDepartamento());
+        System.out.println("O nome do funcionário é "+f.getNome());
+        System.out.println("Cujo CPF corresponde ao número " +f.getCpf());
+        System.out.println("Seu salário é equivalente a R$ " +f.getSalario());
+        System.out.println("A mesma pertence ao setor " + f.getDepartamento() +" da empresa");
     }
     
 }
