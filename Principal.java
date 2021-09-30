523bda0efd51f1a8ccce5bbe7a3c7299ab375c75
carlam97 <carmartins04@hotmail.com>
2021-09-30 19:55:11 +0000

    herança

--- Principal.java
+++ Principal.java
@@ -5,11 +5,13 @@ public class Principal {
         f.setCpf("123.456.789-00");
         f.setSalario(2500);
         f.setDepartamento("Financeiro");
+        
 
         System.out.println("O nome do funcionário é "+f.getNome());
         System.out.println("Cujo CPF corresponde ao número " +f.getCpf());
         System.out.println("Seu salário é equivalente a R$ " +f.getSalario());
         System.out.println("A mesma pertence ao setor " + f.getDepartamento() +" da empresa");
+        System.out.println(f.bonificacao());
     }
     
 }
